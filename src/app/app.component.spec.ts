895fba7e699481604a81419ba8c49f2b0e598e42
MSc Juan José Guzmán Cruz <developersmix@gmail.com>
2022-06-01 10:21:31 -0500

    Project created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'VCard'`, () => {
+  it(`should have as title 'Resume'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VCard');
+    expect(app.title).toEqual('Resume');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('VCard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Resume app is running!');
   });
 });

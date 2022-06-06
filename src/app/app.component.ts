1cf9975a2f15684bb0dd2f9aaa2868b314872d81
M. en C. Juan José Guzmán Cruz <developersmix@gmail.com>
2022-06-06 17:26:56 -0500

    testing with AcquisitionLite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Resume';
+
+  show_option: string = "... more";
+  show_more_about_me: boolean = false;
+  year = new Date().getFullYear();
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  showMoreAboutMe() {
+    this.show_more_about_me = !this.show_more_about_me;
+    if(this.show_more_about_me) {
+      this.show_option = "... less";
+    } else {
+      this.show_option = "... more";
+    }
+    
+  }
+
+  
 }
